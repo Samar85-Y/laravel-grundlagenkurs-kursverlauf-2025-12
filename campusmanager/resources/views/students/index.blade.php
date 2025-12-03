34c25429703676454296816f1d62088ee9b4797c
Samar85-Y <samar85yousef@gmail.com>
2025-12-03 13:07:45 +0000

    feat: Beispiele Tag 3 komplett

--- campusmanager/resources/views/students/index.blade.php
+++ campusmanager/resources/views/students/index.blade.php
@@ -4,32 +4,43 @@
 
 @section('content')
 
-<h2>Studentsliste</h2>
-<p><a href="{{ route('students.create') }}">Neuen Studenten anlegen</a></p>
-@if($students->isEmpty())
-    <p>Es sind noch keine Studenten vorhanden.</p>
+    <h2>Studentenliste</h2>
+
+    <p><a href="{{ route('students.create') }}">Neuen Studenten anlegen</a></p>
+
+    @if($students->isEmpty())
+        <p>Es sind noch keine Studenten vorhanden.</p>
     @else
-    
-<table>
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Vorname</th>
-            <th>Nachname</th>
-            <th>Email</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($students as $s)
-            <tr>
-                <td>{{ $s->id }}</td>
-                <td>{{ $s->firstname }}</td>
-                <td>{{ $s->lastname }}</td>
-                <td>{{ $s->email }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
-@endif
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Vorname</th>
+                    <th>Nachname</th>
+                    <th>Email</th>
+                    <th>Aktion</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($students as $s)
+                    <tr>
+                        <td>{{ $s->id }}</td>
+                        <td>{{ $s->firstname }}</td>
+                        <td>{{ $s->lastname }}</td>
+                        <td>{{ $s->email }}</td>
+                        <td>
+                            <a href="/students/{{ $s->id }}/edit">Bearbeiten</a>
+                            <form action="/students/{{ $s->id }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <input type="hidden" name="student" value="{{ $s->id }}">
+                                <button type="submit">Löschen</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 
 @endsection
--- campusmanager/routes/web.php
+++ campusmanager/routes/web.php
@@ -4,24 +4,40 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StudentsController;
 
-
-
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get('/hello', function(){
     return 'Hallo Laravel!';
 });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
 Route::get('/about', [HomeController::class, 'about'])->name('about');
-Route::get('/students', [StudentsController::class, 'index'])->name('students.index');
 
-Route::get('students/create', [StudentsController::class, 'create']) ->name('students.create');
+Route::get('/students', [StudentsController::class, 'index'])
+    ->name('students.index');
 
-Route::get('/cources', function(){
-return 'Kursliste folgt';
+Route::get('/students/create', [StudentsController::class, 'create'])
+    ->name('students.create');
 
-})->name('course.index');
+Route::post('/students', [StudentsController::class, 'store'])
+    ->name('students.store');
 
+Route::get('/students/{student}', [StudentsController::class, 'show'])
+    ->name('students.show');
+
+Route::get('/students/{student}/edit', [StudentsController::class, 'edit'])
+    ->name('students.edit');
+
+Route::put('/students/{student}', [StudentsController::class, 'update'])
+    ->name('students.update');
+
+Route::delete('/students/{student}', [StudentsController::class, 'destroy'])
+    ->name('students.destroy');
+
+Route::get('/courses', function() {
+    return 'Kursliste folgt';
+})->name('courses.index');
